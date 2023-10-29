0cb1d0769c57ec1a1e8eab7af6cbf6a84a2fb09b
paffkata555 <paffkata_555@abv.bg>
2023-10-29 20:41:29 +0200

    add class

--- src/com/company/_07_ProjectCreation.java
+++ src/com/company/_07_ProjectCreation.java
@@ -1,2 +1,20 @@
-package com.company;public class _07_ProjectCreation {
+package com.company;
+
+import java.util.Scanner;
+
+public class _07_ProjectCreation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String theArchitectName = scanner.nextLine();
+        int numberOfProjects = Integer.parseInt(scanner.nextLine());
+        int requiredHours = numberOfProjects * 3;
+
+
+        //  "The architect {name of architect} will need {needing hours} hours
+        // to complete {count of project} project/s."
+
+        System.out.printf("The architect %s will need %d hours to complete %d project/s.",
+                theArchitectName, requiredHours, numberOfProjects);
+    }
 }
