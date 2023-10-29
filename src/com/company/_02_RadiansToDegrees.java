97eac23815e488a2a6867aee5d00a206391c26f9
paffkata555 <paffkata_555@abv.bg>
2023-10-29 20:02:09 +0200

    remove line

--- src/com/company/_02_RadiansToDegrees.java
+++ src/com/company/_02_RadiansToDegrees.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class _02_RadiansToDegrees {
 
     public static void main(String[] args) {
-       Scanner scanner = new Scanner(System.in);
-       double radians = Double.parseDouble(scanner.nextLine());
+        Scanner scanner = new Scanner(System.in);
+        double radians = Double.parseDouble(scanner.nextLine());
 
-       double degrees = (radians * 180 )/ Math.PI;
+        double degrees = (radians * 180) / Math.PI;
 
         System.out.println(degrees);
     }
