0b0f37d07261b56cdcc55eea36d927c23f09fa50
paffkata555 <paffkata_555@abv.bg>
2023-10-29 21:20:25 +0200

    add few classes

--- src/com/company/_01_ExcellentResult.java
+++ src/com/company/_01_ExcellentResult.java
@@ -1,2 +1,18 @@
-package com.company;public class _01_ExcellentResult {
+package com.company;
+
+import java.util.Scanner;
+
+public class _01_ExcellentResult {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int grade = Integer.parseInt(scanner.nextLine());
+
+        if (grade >= 5) {
+            System.out.println("Excellent!");
+
+
+        }
+
+    }
 }
