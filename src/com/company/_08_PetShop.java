eb9e2ba3516970967b8b079f41431f81a6df8960
paffkata555 <paffkata_555@abv.bg>
2023-10-29 20:47:49 +0200

    add class

--- src/com/company/_08_PetShop.java
+++ src/com/company/_08_PetShop.java
@@ -1,2 +1,20 @@
-package com.company;public class _08_PetShop {
+package com.company;
+
+import java.util.Scanner;
+
+public class _08_PetShop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double numberOfDogFood = scanner.nextDouble();
+        double numberOfCatFood = scanner.nextDouble();
+
+        double finalPrice = (numberOfDogFood * 2.50 + numberOfCatFood * 4.0);
+
+        //  "{final price} lv."
+
+        System.out.printf("%f lv.",finalPrice);
+
+
+    }
 }
--- src/com/company/_09_YardGreening.java
+++ src/com/company/_09_YardGreening.java
@@ -1,2 +1,24 @@
-package com.company;public class _09_YardGreening {
+package com.company;
+
+import java.util.Scanner;
+
+public class _09_YardGreening {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double squareMeterPrice = Double.parseDouble(scanner.nextLine());
+
+        double yardGreening = squareMeterPrice * 7.61;
+        double discount = yardGreening * 0.18;
+        double finalPrice = (yardGreening - discount);
+
+        System.out.printf("The finalPrice is: %f lv.", finalPrice);
+        System.out.println();
+        System.out.printf("The discount is: %f lv.", discount);
+
+        // · "The final price is: {final price } lv."
+        //
+        //· "The discount is: {discount} lv."
+
+    }
 }
