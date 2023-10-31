d58f9587162660b1a7fe47a4b15db84ee09190ff
paffkata555 <paffkata_555@abv.bg>
2023-10-31 22:12:55 +0200

    add new class

--- src/com/company/_01_NumbersEndingIn7.java
+++ src/com/company/_01_NumbersEndingIn7.java
@@ -1,2 +1,15 @@
-package com.company;public class _01_NumbersEndingIn7 {
+package com.company;
+
+
+public class _01_NumbersEndingIn7 {
+    public static void main(String[] args) {
+
+        for (int i = 7; i <= 997; i += 10) {
+
+            if (i % 10 == 7) {
+                System.out.println(i);
+
+            }
+        }
+    }
 }
