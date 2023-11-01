06f3236dcce225c17981b160237a99259cb58e94
paffkata555 <paffkata_555@abv.bg>
2023-11-01 20:52:37 +0200

    add new class

--- src/com/company/_02_HalfSumElement.java
+++ src/com/company/_02_HalfSumElement.java
@@ -1,2 +1,39 @@
-package com.company;public class _02_HalfSumElement {
+package com.company;
+
+import java.util.Scanner;
+
+public class _02_HalfSumElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 1; i <= n; i++) {
+            int number = Integer.parseInt(scanner.nextLine());
+            sum += number;
+
+            if (number > max) {
+                max = number;
+            }
+        }
+
+
+        int sumNoMaxNumber = sum - max;
+
+        if (max == sumNoMaxNumber) {
+            System.out.println("Yes");
+            System.out.println("Sum = " + max);
+
+        } else {
+            int diff = Math.abs(max - sumNoMaxNumber);
+            System.out.println("No");
+            System.out.println("Diff = " + diff);
+
+
+        }
+
+    }
+
 }
--- src/com/company/_03_Histogram.java
+++ src/com/company/_03_Histogram.java
@@ -1,2 +1,47 @@
-package com.company;public class _03_Histogram {
+package com.company;
+
+import java.util.Scanner;
+
+public class _03_Histogram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int p1 = 0;
+        int p2 = 0;
+        int p3 = 0;
+        int p4 = 0;
+        int p5 = 0;
+
+
+        for (int i = 1; i <= n; i++) {
+            int numberCount = Integer.parseInt(scanner.nextLine());
+
+            if (numberCount < 200) {
+                p1++;
+
+            } else if (numberCount >= 200 && numberCount <= 399) {
+                p2++;
+
+            } else if (numberCount >= 400 && numberCount <= 599) {
+                p3++;
+
+            } else if (numberCount >= 600 && numberCount <= 799) {
+                p4++;
+
+            } else if (numberCount >= 800) {
+                p5++;
+            }
+
+
+        }
+
+        System.out.printf("%.2f%%%n", ((1.0 * p1) / n) * 100);
+        System.out.printf("%.2f%%%n", ((1.0 * p2) / n) * 100);
+        System.out.printf("%.2f%%%n", ((1.0 * p3) / n) * 100);
+        System.out.printf("%.2f%%%n", ((1.0 * p4) / n) * 100);
+        System.out.printf("%.2f%%", ((1.0 * p5) / n) * 100);
+
+    }
 }
+
