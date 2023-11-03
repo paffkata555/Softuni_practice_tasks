0068a37e6d7ab2b1a25f17b053bfc04d072eb947
paffkata555 <paffkata_555@abv.bg>
2023-11-03 21:07:23 +0200

    new class

---
+++ src/com/company/_01_ReadText.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class _01_ReadText {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+
+        while (!text.equals("Stop")){
+            System.out.println(text);
+
+            text = scanner.nextLine();
+        }
+    }
+}
--- src/com/company/_08_TennisRanklist.java
+++ src/com/company/_08_TennisRanklist.java
@@ -1,2 +1,40 @@
-package com.company;public class _08_TennisRanklist {
+package com.company;
+
+import java.util.Scanner;
+
+public class _08_TennisRanklist {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numOfTournament = Integer.parseInt(scanner.nextLine());
+        int startingPoints = Integer.parseInt(scanner.nextLine());
+
+
+        int bonusPoints = 0;
+        int wonMatchesCount = 0;
+
+        for (int i = 1; i <= numOfTournament; i++) {
+            String result = scanner.nextLine();
+
+            switch (result) {
+                case "W":
+                    wonMatchesCount++;
+                    bonusPoints += 2000;
+                    break;
+                case "F":
+                    bonusPoints += 1200;
+                    break;
+                case "SF":
+                    bonusPoints += 720;
+                    break;
+            }
+
+
+        }
+
+        System.out.printf("Final points: %d%n", startingPoints + bonusPoints);
+        System.out.printf("Average points: %d%n", bonusPoints / numOfTournament);
+        System.out.printf("%.2f%%", ((1.0 * wonMatchesCount) / numOfTournament) * 100);
+
+    }
+
 }
