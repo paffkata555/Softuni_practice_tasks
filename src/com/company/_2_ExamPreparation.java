a2aaa8907e63cbb2bbf809b968db0301ecd02cf1
paffkata555 <paffkata_555@abv.bg>
2023-11-10 21:21:16 +0200

    new line

---
+++ src/com/company/_2_ExamPreparation.java
@@ -0,0 +1,52 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class _2_ExamPreparation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int badGradesLimit = Integer.parseInt(scanner.nextLine());
+
+
+        String lastProblem = "";
+        int problemsNumber = 0;
+        int sumGrades = 0;
+        int badGradesCount = 0;
+        boolean isBadGradesLimitReached = false;
+
+
+        String command = scanner.nextLine();
+        while (!command.equals("Enough")) {
+            String taskName = command;
+            int grade = Integer.parseInt(scanner.nextLine());
+
+            sumGrades += grade;
+            problemsNumber++;
+            lastProblem = taskName;
+
+
+            if (grade <= 4) {
+                badGradesCount++;
+            }
+
+            if (badGradesCount == badGradesLimit) {
+                isBadGradesLimitReached = true;
+                break;
+
+            }
+
+            command = scanner.nextLine();
+
+        }
+        if (isBadGradesLimitReached) {
+            System.out.printf("You need a break, %d poor grades.", badGradesCount);
+
+        } else {
+            System.out.printf("Average score: %.2f%n" +
+                    "Number of problems: %d%n" +
+                    "Last problem: %s", 1.0 * sumGrades / problemsNumber, problemsNumber, lastProblem);
+        }
+
+    }
+}
+
