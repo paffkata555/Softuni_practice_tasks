68e9edcc57974cac6300eead3cfeb9964901fe1f
paffkata555 <paffkata_555@abv.bg>
2023-11-13 15:25:36 +0200

    add class

--- src/com/company/_04_Walking.java
+++ src/com/company/_04_Walking.java
@@ -7,5 +7,40 @@ public class _04_Walking {
         Scanner scanner = new Scanner(System.in);
 
 
+        int dailyGoal = 10000;
+
+        String command = scanner.nextLine();
+        while (!command.equals("Going home")) {
+
+            int steps = Integer.parseInt(command);
+
+
+            dailyGoal = dailyGoal - steps;
+
+            if (dailyGoal <= 0) {
+                break;
+
+            }
+            command = scanner.nextLine();
+
+        }
+        if (command.equals("Going home")) {
+            int stepsToHome = Integer.parseInt(scanner.nextLine());
+
+            dailyGoal -= stepsToHome;
+        }
+
+        if (dailyGoal <= 0) {
+
+            System.out.printf("Goal reached! Good job!%n" + "%d steps over the goal!", Math.abs(dailyGoal));
+
+
+        } else {
+
+            System.out.printf("%d more steps to reach goal.", Math.abs(dailyGoal));
+
+
+        }
+
     }
 }
---
+++ src/com/company/_05_Coins.java
@@ -0,0 +1,4 @@
+package com.company;
+
+public class _05_Coins {
+}
