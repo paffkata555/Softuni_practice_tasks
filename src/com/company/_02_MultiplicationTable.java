bcaba4cd1c9d8af9daa9c69dfca4c481a91a32fc
paffkata555 <paffkata_555@abv.bg>
2023-11-16 17:15:48 +0200

    add scanner

--- src/com/company/_02_MultiplicationTable.java
+++ src/com/company/_02_MultiplicationTable.java
@@ -3,5 +3,30 @@ package com.company;
 public class _02_MultiplicationTable {
     public static void main(String[] args) {
 
+
+        for (int firstMultiplier = 1; firstMultiplier <= 10; firstMultiplier++) {
+            for (int secondMultiplier = 1; secondMultiplier <= 10; secondMultiplier++) {
+
+                System.out.println(firstMultiplier + " * " + secondMultiplier + " " + "= "
+                        + (firstMultiplier * secondMultiplier));
+
+
+            }
+
+        }
+
     }
 }
+
+//    MULTIPLICATION TABLE VARIANT 2 :
+
+
+//        for (int a = 1; a <= 10; a++) {
+//            for (int b = 1; b <= 10; b++) {
+//                int result = a * b;
+//
+//                System.out.printf("%d * %d = %d%n",a,b,result);
+//
+//            }
+//
+//        }
