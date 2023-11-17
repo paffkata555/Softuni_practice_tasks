8501f22a832d4f0b40b39daf4648d3692bd4dc05
paffkata555 <paffkata_555@abv.bg>
2023-11-17 20:52:20 +0200

    add scanner

--- src/com/company/_03_Combinations.java
+++ src/com/company/_03_Combinations.java
@@ -1,4 +1,33 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class _03_Combinations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int count = 0;
+
+        for (int first = 0; first <= n; first++) {
+            for (int second = 0; second <= n; second++) {
+                for (int third = 0; third <= n; third++) {
+
+                    int sum = first + second + third;
+
+                    if (sum == n) {
+//                        valid combinations
+                        count++;
+                    }
+
+                }
+
+            }
+
+        }
+        System.out.println(count);
+
+
+    }
+
 }
