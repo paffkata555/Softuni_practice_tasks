c4449393a44c4d67a7c3a5259d5596faeb7d9621
paffkata555 <paffkata_555@abv.bg>
2023-11-19 18:34:16 +0200

    add scanner

--- src/com/company/_05_Travelling.java
+++ src/com/company/_05_Travelling.java
@@ -6,7 +6,20 @@ public class _05_Travelling {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String destination = scanner.nextLine();
 
+        while (!destination.equals("End")) {
+            double minimumBudget = Double.parseDouble(scanner.nextLine());
+            double savedMoney = 0;
 
+            while (savedMoney < minimumBudget) {
+                double needingMoney = Double.parseDouble(scanner.nextLine());
+                savedMoney += needingMoney;
+            }
+
+            System.out.printf("Going to %s!%n", destination);
+
+            destination = scanner.nextLine();
+        }
     }
 }
