98921f1d1318b8bac0b601fd74fbca43f4540255
paffkata555 <paffkata_555@abv.bg>
2023-11-19 18:32:35 +0200

    add class

---
+++ src/com/company/_05_Travelling.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class _05_Travelling {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+}
