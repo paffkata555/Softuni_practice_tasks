e7e7feb8deaa800fc5e17ebb125a809cb8c460a9
paffkata555 <paffkata_555@abv.bg>
2023-11-22 17:38:36 +0200

    add scanner

--- src/com/company/_01_NumberPyramid.java
+++ src/com/company/_01_NumberPyramid.java
@@ -5,6 +5,27 @@ import java.util.Scanner;
 public class _01_NumberPyramid {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int numCounter = 1;
+        boolean isBiggerThanN = false;
+
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(numCounter + " ");
+                numCounter++;
+
+                if (numCounter > n) {
+                    isBiggerThanN = true;
+                    break;
+                }
+
+            }
+            if (isBiggerThanN) {
+                break;
+            }
+            System.out.println();
+        }
 
 
     }
