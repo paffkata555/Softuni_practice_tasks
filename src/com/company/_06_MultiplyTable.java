bcc5ce369bb9e1262866106a806ad1547edc6702
paffkata555 <paffkata_555@abv.bg>
2023-12-09 19:49:57 +0200

    add scanner

--- src/com/company/_06_MultiplyTable.java
+++ src/com/company/_06_MultiplyTable.java
@@ -5,6 +5,24 @@ import java.util.Scanner;
 public class _06_MultiplyTable {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
 
+        int thirdDigit = number % 10;
+        int secondDigit = (number / 10) % 10;
+        int firstDigit = number / 100;
+
+        for (int i = 1; i <= thirdDigit; i++) {
+            for (int j = 1; j <= secondDigit; j++) {
+                for (int k = 1; k <= firstDigit; k++) {
+                    int result = i * j * k;
+
+                    System.out.printf("%d * %d * %d  = %d;%n", i, j, k, result);
+                }
+            }
+        }
     }
 }
+
+
+
+
