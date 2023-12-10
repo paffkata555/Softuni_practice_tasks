787c79e899f2adec128d052345f53c7128e66c63
paffkata555 <paffkata_555@abv.bg>
2023-12-10 22:05:05 +0200

    add scanner

--- src/com/company/_01_Moon.java
+++ src/com/company/_01_Moon.java
@@ -6,5 +6,19 @@ public class _01_Moon {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+
+        double speed = Double.parseDouble(scanner.nextLine());
+        double fuelConsumption = Double.parseDouble(scanner.nextLine());
+
+        int distanceToMoon = 384_400;
+        int timeOnMoon = 3;
+
+        double totalDistance = 2 * distanceToMoon;
+        double totalTime = Math.ceil((totalDistance / speed) + timeOnMoon);
+        double totalFuelNeeded = (totalDistance / 100) * fuelConsumption;
+
+        System.out.printf("%d%n", (int) totalTime);
+        System.out.printf("%.0f%n", totalFuelNeeded);
     }
 }
+
