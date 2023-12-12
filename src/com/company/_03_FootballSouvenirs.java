70587d08191a599fc7f351f28227f0762c29caad
paffkata555 <paffkata_555@abv.bg>
2023-12-12 19:19:09 +0200

    add class

---
+++ src/com/company/_03_FootballSouvenirs.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class _03_FootballSouvenirs {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
