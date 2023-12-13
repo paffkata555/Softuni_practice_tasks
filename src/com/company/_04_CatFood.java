c073c23a8179b0edb40eadd2141416ddc02ad6cb
paffkata555 <paffkata_555@abv.bg>
2023-12-13 21:32:50 +0200

    add class

---
+++ src/com/company/_04_CatFood.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class _04_CatFood {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
