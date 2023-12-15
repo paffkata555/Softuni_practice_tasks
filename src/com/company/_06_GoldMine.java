5b8de6b3f161e98971ffb70689bc7816733f0be1
paffkata555 <paffkata_555@abv.bg>
2023-12-15 19:28:20 +0200

    add scanner

--- src/com/company/_06_GoldMine.java
+++ src/com/company/_06_GoldMine.java
@@ -1,7 +1,33 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class _06_GoldMine {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int locations = Integer.parseInt(scanner.nextLine());
 
+        for (int i = 0; i < locations; i++) {
+            double expectedAverageGoldPerDay = Double.parseDouble(scanner.nextLine());
+            int days = Integer.parseInt(scanner.nextLine());
+
+            double totalGold = 0;
+
+            for (int day = 0; day < days; day++) {
+                double goldPerDay = Double.parseDouble(scanner.nextLine());
+                totalGold += goldPerDay;
+            }
+
+            double averageGoldPerDay = totalGold / days;
+
+            if (averageGoldPerDay >= expectedAverageGoldPerDay) {
+                System.out.printf("Good job! Average gold per day: %.2f.%n", averageGoldPerDay);
+                System.out.printf("You need %.2f gold.%n", averageGoldPerDay / days);
+
+            } else {
+                double neededGold = expectedAverageGoldPerDay * days - totalGold;
+                System.out.printf("You need %.2f gold.%n", neededGold);
+            }
+        }
     }
 }
