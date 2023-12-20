c53973cbcc1bc1543e0b5031ff3d883114672c1d
paffkata555 <paffkata_555@abv.bg>
2023-12-20 21:28:09 +0200

    add scanner

--- src/com/company/AgencyProfit.java
+++ src/com/company/AgencyProfit.java
@@ -5,5 +5,24 @@ import java.util.Scanner;
 public class AgencyProfit {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        String airlineName = scanner.nextLine();
+        int adultTicketsCount = Integer.parseInt(scanner.nextLine());
+        int childTicketsCount = Integer.parseInt(scanner.nextLine());
+        double adultTicketPrice = Double.parseDouble(scanner.nextLine());
+        double serviceFee = Double.parseDouble(scanner.nextLine());
+
+        double totalAdultPrice = adultTicketsCount * adultTicketPrice;
+        double totalChildPrice = childTicketsCount * (adultTicketPrice - (adultTicketPrice * 0.7));
+
+        double totalTicketPrice = totalAdultPrice + totalChildPrice + (serviceFee * (adultTicketsCount + childTicketsCount));
+        double agencyProfit = 0.2 * totalTicketPrice;
+
+        System.out.printf("The profit of your agency from %s tickets is %.2f lv.", airlineName, agencyProfit);
     }
 }
+
+
+
+
+
