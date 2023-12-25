27226fa5d5045f5235b5c99cfe4568f0150fdd0f
paffkata555 <paffkata_555@abv.bg>
2023-12-25 20:40:05 +0200

    add scanner

--- src/com/company/BasketballTournament.java
+++ src/com/company/BasketballTournament.java
@@ -1,7 +1,12 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class BasketballTournament {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
 
     }
 }
