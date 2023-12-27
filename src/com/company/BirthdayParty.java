6f30effbd39bc16aca5491f46837b3285111985d
paffkata555 <paffkata_555@abv.bg>
2023-12-27 20:16:11 +0200

    add scanner

--- src/com/company/BirthdayParty.java
+++ src/com/company/BirthdayParty.java
@@ -6,5 +6,21 @@ public class BirthdayParty {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        double rentOfTheHall = Double.parseDouble(scanner.nextLine());
+
+        double cakePrice = rentOfTheHall * 0.20;
+        double drinkPrice = cakePrice - (cakePrice * 0.45);
+        double animatorPrice = rentOfTheHall / 3;
+
+        double budget = rentOfTheHall + cakePrice + drinkPrice + animatorPrice;
+
+        System.out.println(budget);
+
+
     }
 }
+//Торта – цената ѝ е 20% от наема на залата
+//• Напитки – цената им е 45% по-малко от тази на тортата
+//• Аниматор – цената му е 1/3 от цената за наема на залат
+
+
